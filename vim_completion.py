22e4d4c5010852cd480ef63cb2506e394cd0bc97
Evan Mallory <evan@duolingo.com>
2015-09-23 12:33:25 -0400

    Show line in status bar, bug fixes

--- vim_completion.py
+++ vim_completion.py
@@ -3,10 +3,10 @@
 
 # Things still to do:
 #   complete more
-#   show matched line in status bar
 #   highlight matched word in the view
 #   only complete unique (maybe make this an option? could be annoying with complete more)
 
+import sublime
 import sublime_plugin
 
 
@@ -18,11 +18,15 @@ last_search_pos = None
 
 def complete_word(view, edit, do_previous):
     global last_initial_pos, last_word_at_ipos, last_search_pos
+
+    # Compute the region from the beginning of the word containing the cursor
+    # through the cursor.
     initial_word_region = view.word(view.sel()[0])
+    initial_word_region = sublime.Region(initial_word_region.a, view.sel()[0].a)
     word = view.substr(initial_word_region)
 
     if (initial_word_region.a == last_initial_pos and last_word_at_ipos is not None and
-            last_search_pos is not None):
+            word.startswith(last_word_at_ipos) and last_search_pos is not None):
         position = last_search_pos
         word = last_word_at_ipos
     else:
@@ -36,7 +40,7 @@ def complete_word(view, edit, do_previous):
     if (do_previous and position > 0) or (not do_previous and position < view.size()):
         position += -1 if do_previous else 1
 
-    print("word: %s; position: %d" % (word, position))
+    # sublime.status_message("word: %s; position: %d" % (word, position))
 
     size_checked = 0
     match_found = False
@@ -45,8 +49,10 @@ def complete_word(view, edit, do_previous):
         compare_word_region = view.word(position)
         compare_word = view.substr(compare_word_region)
 
-        # Found a match; replace current word
-        if compare_word.startswith(word):
+        # If we find a word that starts with the proper prefix, it's a match!
+        # But don't count it if we have just found our starting place.
+        if compare_word.startswith(word) and compare_word_region.a != view.word(view.sel()[0]).a:
+            # replace word
             view.replace(edit, initial_word_region, compare_word)
             match_found = True
 
@@ -56,6 +62,13 @@ def complete_word(view, edit, do_previous):
             position = compare_word_region.end() + 1
 
         size_checked += compare_word_region.size()
+
+    if match_found:
+        sublime.status_message(
+            "Completion line: %s" %
+            view.substr(view.line(sublime.Region(position, position))))
+    else:
+        sublime.status_message("No completion found")
 
     last_search_pos = position
 
