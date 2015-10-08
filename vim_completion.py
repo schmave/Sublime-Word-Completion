8bdf843a3dac8c170a01156578c46afa48e9d8be
Evan Mallory <evan@duolingo.com>
2015-10-08 16:52:46 -0400

    Make complete_word complete through the end of the next word, stopping at line end

--- vim_completion.py
+++ vim_completion.py
@@ -2,9 +2,9 @@
 # Posted on SO: http://stackoverflow.com/questions/15260843
 
 # Things still to do:
-#   complete more
 #   only complete unique (maybe make this an option? could be annoying with complete more)
 #   complete from other open buffers, MRU first
+#   detect if user typing has occurred since last completion (last modified time or text at cursor?)
 
 import sublime
 import sublime_plugin
@@ -29,6 +29,7 @@ complete_more_pos = None
 
 REGION_KEY = 'VimCompletion'
 DRAW_FLAGS = sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE | sublime.DRAW_SOLID_UNDERLINE
+
 
 def complete_word(view, edit, do_previous):
     global last_initial_pos, last_word_at_ipos, last_search_pos, last_view,\
@@ -118,15 +119,46 @@ class CompleteNextWordCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         complete_word(self.view, edit, False)
 
+
 class CompleteMoreCommand(sublime_plugin.TextCommand):
+    def debug(self):
+        classes = {
+            sublime.CLASS_WORD_START: 'CLASS_WORD_START',
+            sublime.CLASS_WORD_END: 'CLASS_WORD_END',
+            sublime.CLASS_PUNCTUATION_START: 'CLASS_PUNCTUATION_START',
+            sublime.CLASS_PUNCTUATION_END: 'CLASS_PUNCTUATION_END',
+            sublime.CLASS_SUB_WORD_START: 'CLASS_SUB_WORD_START',
+            sublime.CLASS_SUB_WORD_END: 'CLASS_SUB_WORD_END',
+            sublime.CLASS_LINE_START: 'CLASS_LINE_START',
+            sublime.CLASS_LINE_END: 'CLASS_LINE_END',
+            sublime.CLASS_EMPTY_LINE: 'CLASS_EMPTY_LINE'
+        }
+
+        # for val in classes:
+        #     print("%d %s" % (val, classes[val]))
+
+
+        c = self.view.classify(self.view.sel()[0].begin())
+
+        result = ''
+        for val in classes:
+            if val & c > 0:
+                result += classes[val] + " "
+
+        print("%d %s" % (c, result))
+
+
     def run(self, edit):
+        # self.debug()
+        # return
+
         global last_initial_pos, last_word_at_ipos, last_search_pos, last_view,\
                 last_search_was_previous, complete_more_pos
 
         view = self.view
 
-        if last_initial_pos:
-            print("starting complete more, last initial pos %d" % last_initial_pos)
+        # if last_initial_pos:
+        #     print("starting complete more, last initial pos %d" % last_initial_pos)
 
         if not complete_more_pos:
             return
@@ -135,35 +167,18 @@ class CompleteMoreCommand(sublime_plugin.TextCommand):
         last_view = last_search_was_previous = None
 
         cur_pos = view.sel()[0].begin()
-        print("character to right of complete_more_pos: %r, class %d" % (
-            view.substr(complete_more_pos), view.classify(complete_more_pos)))
+        # print("character to right of complete_more_pos: %r, class %d" % (
+        #     view.substr(complete_more_pos), view.classify(complete_more_pos)))
 
-        punctuation_copied = False
+        # Copy to the end of the line or the end of the next word
         count = 0
-        while count < 5 and (
-                (view.classify(complete_more_pos) & sublime.CLASS_PUNCTUATION_START)):
-            print("class %d %d" % (
-                view.classify(complete_more_pos), sublime.CLASS_PUNCTUATION_START))
+        while (view.classify(complete_more_pos) & sublime.CLASS_LINE_END) == 0:
             count += 1
-            punctuation_copied = True
             view.insert(edit, cur_pos, view.substr(complete_more_pos))
             cur_pos += 1
             complete_more_pos += 1 if complete_more_pos < cur_pos else 2
 
-        if punctuation_copied:
-            return
-
-        word_region = view.word(complete_more_pos)
-        # print("word region start %d word region end %d" % (word_region.begin(), word_region.end()))
-        text = view.substr(word_region)
-        print("next word to complete %r" % (text))
-        num_inserted = view.insert(edit, cur_pos, text)
-
-        complete_more_pos = word_region.end()
-        # If we are copying from an area after the cursor, we need to add
-        # an additional amount to compensate for the fact that inserting text
-        # changes the index of all text after the insertion point.
-        if complete_more_pos > cur_pos:
-            complete_more_pos += num_inserted
-
-        # print ("new complete more pos %d" % complete_more_pos)
+            # max of 500 characters so that we don't loop forever due to an error
+            if (count >= 500 or
+                (view.classify(complete_more_pos) & sublime.CLASS_WORD_END)):
+                break
